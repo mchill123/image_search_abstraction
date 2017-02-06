7fe843bdc5c7975f1b6cc9d20382699a645f1309
Mckinley <mckinley.s.hill@gmail.com>
2017-02-06 19:26:00 +0000

    T

--- api.js
+++ api.js
@@ -42,7 +42,7 @@ module.exports= function(app, db){
         var obj = [];
         for(var i=0;i<10;i++){
             var hit = {
-                'url': data.items[i].pagemap.cse_image.src
+                'url': data.items[i].pagemap.cse_image
             };
             obj.push(hit);
         }
