ed12fd507c32881a9fabd141bcf85f86c80f4fd8
Mckinley <mckinley.s.hill@gmail.com>
2017-02-08 15:08:36 +0000

    T

--- api.js
+++ api.js
@@ -24,12 +24,12 @@ module.exports= function(app, db){
             offset = 1;
         }else offset = req.query.offset*10;
         
-      request('GET https://www.googleapis.com/customsearch/v1?q='+ search +'&cx=005494827447375698070:kljageccrgu&num=10&searchType=image&start='+offset+'&fields=context%2Citems%2Cpromotions&key=AIzaSyBrzh-HsmfFBuMJXycWFKWHhmOTVbMjEdc', function(err, response, data){
+      request('https://www.googleapis.com/customsearch/v1?q='+ search +'&cx=005494827447375698070:kljageccrgu&num=10&searchType=image&start='+offset+'&fields=context%2Citems%2Cpromotions&key=AIzaSyBrzh-HsmfFBuMJXycWFKWHhmOTVbMjEdc', function(err, response, data){
           if (err){
-              console.log(err)
+              console.log(err);
           }
           res.send(data);
-      })
+      });
       
       
             
