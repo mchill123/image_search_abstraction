f64122c1c892d0c55ac6fd0fd7444174e86c23da
Mckinley <mckinley.s.hill@gmail.com>
2017-02-08 17:04:46 +0000

    T

--- api.js
+++ api.js
@@ -30,7 +30,7 @@ module.exports= function(app, db){
           }
           var a= JSON.parse(data);
           console.log(a);
-        res.send(a);
+        res.send(a.items);
        
          
       });
